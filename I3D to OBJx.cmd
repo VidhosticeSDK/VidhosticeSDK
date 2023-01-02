6b4309f4150fc1ba0845a32e07ae872ef93f89c1
VidhosticeSDK <106232621+VidhosticeSDK@users.noreply.github.com>
2023-01-02 01:00:08 +0100

    Add files via upload

--- I3D to OBJx.cmd
+++ I3D to OBJx.cmd
@@ -4,21 +4,27 @@ color a
 cls
 type %VidhosticeSDK%\info.txt
 
-if not exist "%1" goto error
+if not exist "%~1" goto error
 
 :loop
- 
+
 if %~x1==.shapes (
-    echo Shape file: "%1"
-    %VidhosticeSDK%\I3DShapesTool.exe -v "%1"
+    echo Shape file "%~1"
+    %VidhosticeSDK%\I3DShapesTool.exe -v "%~1"
+    echo.
+    if exist "%~n1" (
+        %VidhosticeSDK%\i3d_to_mtl.py "%~n1"
+    )
 ) else (
-    echo Shape file: "%1.shapes"
-    %VidhosticeSDK%\I3DShapesTool.exe -v "%1.shapes"
+    echo Shape file "%~1.shapes"
+    %VidhosticeSDK%\I3DShapesTool.exe -v "%~1.shapes"
+    echo.
+    %VidhosticeSDK%\i3d_to_mtl.py "%~1"
 )
 echo.
 
 shift
-if exist %1 goto loop
+if exist "%~1" goto loop
 
 goto end
 
